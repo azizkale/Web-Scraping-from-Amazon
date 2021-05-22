491ce2479456353cbb5a6234a335efffbbb5fd7e
azizkale <azizkale@hotmail.com>
2021-05-22 10:19:56 +0200

    modelAdded

--- src/Routes/products.js
+++ src/Routes/products.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const products = express.Router();
+const Product = require("../models/Product");
 const axios = require("axios");
 const cheerio = require("cheerio");
 
 products.route("/").get((req, res, next) => {
   axios
     .get(
-      "https://www.amazon.com.tr/b/ref=sv_ap_gender_4_6_1_1?node=13546667031"
+      "https://www.amazon.com.tr/gp/bestsellers/apparel/13546667031/ref=zg_bs_nav_a_2_13546647031"
     )
     .then((response) => {
       const $ = cheerio.load(response.data);
 
-      console.log($(".octopus-best-seller-card > div > span").text());
+      // console.log($(".octopus-best-seller-card > div > span").text());
+      Product.pCategory = $(".zg-banner-text").text();
+      Product.pTitle = $("h1 > span").text();
+      Product.pImage = $("li > img").text();
+      Product.pName = $("li > .zg-item > a > .p13n-sc-truncated").innerHTML;
+      console.log(Product);
     })
     .catch((error) => {
       console.error(error);
---
+++ src/models/Product.js
@@ -0,0 +1,9 @@
+const Product = (pCategory, pTitle, pImage, pName, pPrice) => {
+  this.pCategory = pCategory;
+  this.pTitle = pTitle;
+  this.pImage = pImage;
+  this.pName = pName;
+  this.pPrice = pPrice;
+};
+
+module.exports = Product;
