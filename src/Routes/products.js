fd15a60ab96ec1badc0aadd008d80ba4d021c375
azizkale <azizkale@hotmail.com>
2021-05-23 07:42:31 +0200

    gettingDetailsOK

--- src/Routes/products.js
+++ src/Routes/products.js
@@ -4,23 +4,41 @@ const Product = require("../models/Product");
 const axios = require("axios");
 const cheerio = require("cheerio");
 
+let pList = new Product();
+
 products.route("/").get((req, res, next) => {
+  //gets products page
   axios
     .get(
-      "https://www.amazon.com.tr/gp/bestsellers/apparel/13546667031/ref=zg_bs_nav_a_2_13546647031"
+      "https://www.amazon.com.tr/s?bbn=12466209031&rh=n%3A12466208031%2Cn%3A13546647031&dc&qid=1621681498&rnid=12466209031&ref=lp_12466209031_nr_n_2"
     )
     .then((response) => {
       const $ = cheerio.load(response.data);
+      //gets links of products
+      $(".sg-col-inner").map((index, prd) => {
+        // gets products details page
 
-      // console.log($(".octopus-best-seller-card > div > span").text());
-      Product.pCategory = $(".zg-banner-text").text();
-      Product.pTitle = $("h1 > span").text();
-      Product.pImage = $("li > img").text();
-      Product.pName = $("li > .zg-item > a > .p13n-sc-truncated").innerHTML;
-      console.log(Product);
+        let oneProduct = new Product();
+
+        oneProduct.pLink =
+          "https://www.amazon.com.tr" + $(prd).find($("a")).attr("href");
+
+        axios
+          .get(`https://www.amazon.com.tr${$(prd).find($("a")).attr("href")}`)
+          .then((response2) => {
+            const $ = cheerio.load(response2.data);
+
+            oneProduct.pTitle = $("#productTitle").text().trim();
+            oneProduct.pStar = $("i > span").text().split(",")[0];
+            console.log(oneProduct);
+          })
+          .catch((error2) => {
+            // console.log(error2);
+          });
+      });
     })
     .catch((error) => {
-      console.error(error);
+      // console.error(error);
     });
 });
 
--- src/models/Product.js
+++ src/models/Product.js
@@ -1,9 +1,12 @@
-const Product = (pCategory, pTitle, pImage, pName, pPrice) => {
-  this.pCategory = pCategory;
-  this.pTitle = pTitle;
-  this.pImage = pImage;
-  this.pName = pName;
-  this.pPrice = pPrice;
-};
+class Product {
+  constructor(pLink, pTitle, pStar, pImage, pName, pPrice) {
+    this.pLink = pLink;
+    this.pTitle = pTitle;
+    this.pStar = pStar;
+    this.pImage = pImage;
+    this.pName = pName;
+    this.pPrice = pPrice;
+  }
+}
 
 module.exports = Product;
