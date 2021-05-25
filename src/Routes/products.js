8177c78e7cdd594fe6342752c6bc6710be0cce6b
azizkale <azizkale@hotmail.com>
2021-05-25 17:20:59 +0200

    settimeout

--- src/Routes/products.js
+++ src/Routes/products.js
@@ -22,43 +22,51 @@ products.route("/").get((req, res, next) => {
       });
       // gets product details
       for (let i = 0; i < linkList.length; i++) {
-        axios
-          .get("https://www.amazon.com.tr" + linkList[i])
-          .then(async (response2) => {
-            const $ = await cheerio.load(response2.data);
-
-            oneProduct.pLink = "https://www.amazon.com.tr" + linkList[i];
-            oneProduct.pTitle = $("#productTitle").text().trim();
-            oneProduct.pPrice = $("#priceblock_ourprice").text();
-            oneProduct.pAvailability = $("#availability > span").text().trim();
-            oneProduct.pCompanyName = $("a#bylineInfo").text();
-
-            let arr = [];
-            $("#twister")
-              .find($("#variation_color_name > ul > li"))
-              .map(function (i, el) {
-                // this === el
-                return arr.push($(this).find($("img")).attr("alt"));
-              });
-            arr.push(
-              $("#variation_color_name").find($("span.selection")).text().trim()
-            );
-            oneProduct.pColor = arr;
-
-            oneProduct.pSize = $("#twister > #variation_size_name")
-              .find($("span.selection"))
-              .text()
-              .trim();
-            console.log(oneProduct);
-          })
-          .catch((error) => {
-            console.error("error");
-          });
+        (function (index) {
+          setTimeout(function () {
+            getDetails(linkList[i]);
+          }, i * 4000);
+        })(i);
       }
     })
     .catch((error) => {
       // console.error(error);
     });
 });
+
+function getDetails(link) {
+  axios
+    .get("https://www.amazon.com.tr" + link)
+    .then(async (response2) => {
+      const $ = await cheerio.load(response2.data);
+
+      oneProduct.pLink = "https://www.amazon.com.tr" + link;
+      oneProduct.pTitle = $("#productTitle").text().trim();
+      oneProduct.pPrice = $("#priceblock_ourprice").text();
+      oneProduct.pAvailability = $("#availability > span").text().trim();
+      oneProduct.pCompanyName = $("a#bylineInfo").text();
+
+      let arr = [];
+      $("#twister")
+        .find($("#variation_color_name > ul > li"))
+        .map(function (i, el) {
+          // this === el
+          return arr.push($(this).find($("img")).attr("alt"));
+        });
+      arr.push(
+        $("#variation_color_name").find($("span.selection")).text().trim()
+      );
+      oneProduct.pColor = arr;
+
+      oneProduct.pSize = $("#twister > #variation_size_name")
+        .find($("span.selection"))
+        .text()
+        .trim();
+      console.log(oneProduct);
+    })
+    .catch((error) => {
+      console.error("error");
+    });
+}
 
 module.exports = products;
